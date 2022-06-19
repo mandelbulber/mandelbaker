bfd5c9fa8e1d4079e593d245a30d6f5389793c14
Andrin Eugster <aeugster@online.gibz.ch>
2022-06-19 21:13:35 +0200

    Minor refactoring

--- src/mandelbaker.py
+++ src/mandelbaker.py
@@ -1,27 +1,28 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# -----------------------------------------*-General-Information-*------------------------------------------
+# |---------------------------------------.*-General-Information-*.----------------------------------------|
 # Created By  : Eugster Andrin
 # Created Date: 09/06/2022
 # version ='1.0'
-# -----------------------------------------*-Program-Description-*------------------------------------------
+# |---------------------------------------.*-Program-Description-*.----------------------------------------|
 """ This module represents a calculator used to render manderbrot fractals.                              """
 """ To achieve the perfect mandelbrot of your personal liking, feel free to adjust the variables below.  """
 """ Be aware that this program is still WIP, so please don't set your expectations too high.             """
 """ There are still some (especially performance) improvements to be done, but I hope you can already    """
 """ enjoy the current state of this project.                                                             """
 """ For further information, have a look at the README.md.                                               """
-# ----------------------------------------------*-Libraries-*-----------------------------------------------
+# |--------------------------------------------.*-Libraries-*.---------------------------------------------|
+import os
+import datetime
 from PIL import Image, ImageDraw
 from multiprocessing import Process, Queue
-import datetime
-# -------------------------------------------*-User-Adjustments-*-------------------------------------------
+# |-----------------------------------------.*-User-Adjustments-*.-----------------------------------------|
 height_of_mandelbrot = 512 #   512 | 1080 | 3840 | 7680 | 15360 | 30720 | 61440 | 122880 | 245760 | 491520 |
 width_of_mandelbrot  = 512 #              |  4K  |  8K  |  16K  |  32K  |  64K  |  128K  |  256K  |  512K  |
 max_iterations = 255
 number_of_processes = 1 # not integrated yet
 results_folder = "C:\mandelbaker\\results"
-# ----------------------------------------------------------------------------------------------------------
+# |--------------------------------------------------------------------------------------------------------|
 
 def mandelbrot_calc():
     imageQuartalsList = [(0, 0), (width_of_mandelbrot//2, 0), (0, height_of_mandelbrot//2), (width_of_mandelbrot//2, height_of_mandelbrot//2)]
@@ -66,7 +67,6 @@ def partialcalculator(queue, startX, startY):
 
 def draw(calcParam):
     print("start drawing at: " + str(datetime.datetime.now()))
-    # arr1, arr2, arr3, arr4 = arrays
     lists, quartals = calcParam
     image = Image.new("RGB", (width_of_mandelbrot, height_of_mandelbrot))
     
@@ -75,15 +75,13 @@ def draw(calcParam):
         xtemp = xytemp[0]
         ytemp = xytemp[1]
         for sublist in list:
-            for index, i in enumerate(sublist):  #enumerate(list):
-                imgY = 0
+            for index, i in enumerate(sublist):
                 x = 0
                 y = 0
                 x = xtemp + index % (width_of_mandelbrot//2)
                 y = ytemp + index // (width_of_mandelbrot//2)
                 image.putpixel((x, y), (i % 3 * 64, i % 8 * 32, i % 16 * 16))
     return image
-    
 
 def program():
     start_time = datetime.datetime.now()
@@ -92,6 +90,8 @@ def program():
     duration_of_calculation = end_time - start_time
     timestamp = ImageDraw.Draw(image)
     timestamp.text((0, 0), str(duration_of_calculation))
+    if not os.path.exists(results_folder):
+        os.makedirs(results_folder)
     image.save(results_folder + "\mandelbrot_" + str(width_of_mandelbrot) + "x" + str(height_of_mandelbrot) + ".png", "PNG")
     image.show()
 
