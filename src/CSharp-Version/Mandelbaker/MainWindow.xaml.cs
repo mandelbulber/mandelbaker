01b5be2ff39b7599abde1f89f263bcc159abdfa0
TimArnoldd <arnold.tim2005@gmail.com>
2022-08-14 21:00:35 +0200

    C#-Version

---
+++ src/CSharp-Version/Mandelbaker/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using Mandelbaker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+using System.IO;
+using Mandelbaker.ViewModels;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Mandelbaker
+{
+    public partial class MainWindow : Window
+    {
+        MandelbrotGeneratorViewModel _viewModel;
+        public MainWindow()
+        {
+            _viewModel = new();
+            DataContext = _viewModel;
+            InitializeComponent();
+        }
+
+        private void RenderMandelbrot(object sender, RoutedEventArgs e)
+        {
+            MandelbrotCalculationInformation mci;
+
+            if (_viewModel.Method == "CPU")
+            {
+                mci = Mandelbrot.SaveCPUMandelbrot(_viewModel.Resolution, _viewModel.Iterations, _viewModel.XLeft, _viewModel.YTop, _viewModel.Zoom, _viewModel.Directory, _viewModel.Filename);
+            }
+            else
+            {
+                mci = Mandelbrot.SaveGPUMandelbrot(_viewModel.Resolution, _viewModel.Iterations, _viewModel.XLeft, _viewModel.YTop, _viewModel.Zoom, _viewModel.Directory, _viewModel.Filename);
+            }
+            _viewModel.SetOutput("Render complete: " + mci.ToString());
+        }
+        private void RenderMatrix(object sender, RoutedEventArgs e)
+        {
+            MandelbrotCalculationInformation mci;
+            List<MandelbrotCalculationInformation> mcis;
+
+            (mci, mcis) = Mandelbrot.RenderMatrix(_viewModel.Resolution, _viewModel.Iterations, _viewModel.DimensionSize, _viewModel.XLeft, _viewModel.YTop, _viewModel.Zoom, _viewModel.Directory, _viewModel.Method == "GPU" ? true : false);
+            _viewModel.SetOutput("Render complete: " + mci.ToString());
+
+            string jsonString = JsonConvert.SerializeObject((mci, mcis), Formatting.Indented);
+
+            Directory.CreateDirectory(@"C:\Mandelbaker\CalculationInformation\");
+            DateTime now = DateTime.Now;
+            string date = now.ToString("dd-MM-yyyy_HH-mm-ss");
+            string jsonFilename = @$"C:\Mandelbaker\CalculationInformation\Matrix_{_viewModel.Resolution}px_{date}.json";
+            File.WriteAllText(jsonFilename, jsonString);
+        }
+        private void OpenFolder(object sender, RoutedEventArgs e)
+        {
+            using (var dialog = new FolderBrowserDialog())
+            {
+                DialogResult result = dialog.ShowDialog();
+
+                if (result == System.Windows.Forms.DialogResult.OK)
+                {
+                    _viewModel.Directory = dialog.SelectedPath;
+                }
+            }
+        }
+    }
+}
---
+++ src/CSharp-Version/Mandelbaker/ViewModels/MandelbrotGeneratorViewModel.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Mandelbaker.ViewModels
+{
+    public class MandelbrotGeneratorViewModel : INotifyPropertyChanged
+    {
+        // Generic Properties
+        public int Resolution { get; set; } = 1000;
+        public int Iterations { get; set; } = 255;
+        public double XLeft { get; set; } = -2;
+        public double YTop { get; set; } = 1.5;
+        public double Zoom { get; set; } = 1;
+        private string _directory = string.Empty;
+        public string Directory
+        {
+            get => _directory;
+            set
+            {
+                _directory = value;
+                if (value.Last() != '/' && value.Last() != '\\')
+                    _directory += @"\";
+                NotifyPropertyChanged();
+            }
+        }
+        public string Method { get; set; }
+        public List<string> Methods { get; set; }
+        private string _output = string.Empty;
+        public string Output
+        {
+            get => _output;
+            set
+            {
+                _output = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public async void SetOutput(string output)
+        {
+            Output = output;
+            await Task.Delay(5000);
+            Output = string.Empty;
+        }
+
+        // Single Image Properties
+        private string? _filename;
+        public string? Filename
+        {
+            get => _filename;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    _filename = null;
+                    return;
+                }
+
+                if (value.EndsWith(".png"))
+                {
+                    if (value.Length > 4)
+                    {
+                        _filename = value;
+                    }
+                }
+                else
+                {
+                    _filename = value + ".png";
+                }
+            }
+        }
+
+        // Matrix Properties
+        private int _dimensionSize = 5;
+        public int DimensionSize
+        {
+            get => _dimensionSize;
+            set
+            {
+                if (value >= 1)
+                {
+                    _dimensionSize = value;
+                }
+            }
+        }
+
+
+        public MandelbrotGeneratorViewModel()
+        {
+            Methods = new()
+            {
+                "CPU",
+                "GPU"
+            };
+            Method = "CPU";
+            Directory = @"C:\Mandelbaker\Output\";
+        }
+
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
